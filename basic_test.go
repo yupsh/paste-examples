6a657c3d0d83286886aa7ceae1fb1433eba08060
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:02 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -1,17 +1,16 @@
 package paste_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/paste"
 )
 
 func ExamplePaste_basic() {
 	// paste file1.txt file2.txt
 	// Note: This requires actual files to paste together
-	yup.MustRun(
+	gloo.MustRun(
 		Paste("testdata/file1.txt", "testdata/file2.txt"),
 	)
 	// Output:
 	//
 }
-
